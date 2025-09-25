25847da1346ecc45eb0de8dd7d0e7e602f310bba
Kelsey Goli <you@example.com>
2025-09-25 14:53:30 +0000

    Lab 3 Changes

--- settings/core.php
+++ settings/core.php
@@ -1,4 +1,25 @@
 <?php
 // Core initialization file
 require_once(__DIR__ . "/db_class.php");
+
+// Start the session
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+function isLoggedIn()
+{
+    if (isset($_SESSION['user_id'])) {
+        return true;
+    }
+    return false;
+}
+
+function isAdmin()
+{
+    if (is_logged_in() && isset($_SESSION['user_role']) && $_SESSION['user_role'] == 1) {
+        return true;
+    }
+    return false;
+}
 ?>
