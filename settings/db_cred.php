811e0875b2ee8351a508a7bf466ba4091d93c6af
Kelsey Goli <you@example.com>
2025-10-02 10:34:31 +0000

    DB Cred Changes

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,7 +1,7 @@
 <?php
 // Database credentials
-define("DB_HOST", "localhost");
-define("DB_USER", "root");     
-define("DB_PASS", "");         
-define("DB_NAME", "lb_shoppn");
+define("DB_HOST", "http://169.239.251.102:442/");
+define("DB_USER", "kelsey.goli");     
+define("DB_PASS", "KSG@sql25");         
+define("DB_NAME", "ecommerce_2025A_kelsey_goli");
 ?>
